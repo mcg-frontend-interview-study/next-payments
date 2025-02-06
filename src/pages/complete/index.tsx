68a5a635be035b03cd5e69715e0e09535ca0419b
김진호 <rlawlsgh1227@gmail.com>
2025-02-06 14:33:25 +0900

    feat: 완료페이지에서 버튼 누르면 다시 등록페이지로 이동

--- src/pages/complete/index.tsx
+++ src/pages/complete/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import '../../styles/globals.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Complete() {
   const firstCardNumber = '5511';
@@ -19,9 +20,12 @@ export default function Complete() {
           <Image src="/images/check.svg" alt="complete" width={32} height={20} />
         </div>
         <p className="font-bold text-2xl/7 text-center whitespace-pre-line">{`${firstCardNumber}로 시작하는\n${cardBrand}가 등록되었어요.`}</p>
-        <button className="w-full py-4 bg-gray rounded-md text-center font-bold text-white2 text-base/3 hover:bg-grayVariants active:bg-grayVariants">
+        <Link
+          href="/"
+          className="w-full py-4 bg-gray rounded-md text-center font-bold text-white2 text-base/3 hover:bg-grayVariants active:bg-grayVariants"
+        >
           확인
-        </button>
+        </Link>
       </main>
     </>
   );
