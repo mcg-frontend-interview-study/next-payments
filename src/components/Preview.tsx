da61437bb53684b307c03e947f93e4d7b5143c39
김진호 <rlawlsgh1227@gmail.com>
2025-02-06 13:43:14 +0900

    feat: 카드사 이미지 조건에 따라 다르게 보여주는 기능

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -6,25 +6,40 @@ type PreviewProps = {
   cardName: string;
 };
 
+const cardCompany = {
+  isVisa: (firstNumber: string) => firstNumber.startsWith('4'),
+  isMasterCard: (firstNumber: string) => {
+    const frontTwoNumber = Number(firstNumber.slice(0, 2));
+    return frontTwoNumber >= 51 && frontTwoNumber <= 55;
+  },
+};
+
+const getCardImage = (firstNumber: string) => {
+  if (cardCompany.isVisa(firstNumber)) return '/images/visa.svg';
+  if (cardCompany.isMasterCard(firstNumber)) return '/images/mastercard.svg';
+};
+
 export const Preview = ({cardNumbers, expirationDate, cardName}: PreviewProps) => {
+  const cardImage = getCardImage(cardNumbers[0]);
+
   return (
     <article className="flex flex-col w-56 h-32 bg-gray drop-shadow-md rounded-[4]" aria-label="카드 미리보기">
       <header className="flex justify-between items-center w-full px-3 py-2">
         <Image src="/images/icChip.svg" alt="icChip" width={36} height={22} />
-        <Image src="/images/mastercard.svg" alt="icChip" width={36} height={22} />
+        {cardImage && <Image src={cardImage} alt="card company" width={36} height={22} />}
       </header>
       <section className="flex flex-col w-full px-4 gap-2" aria-label="카드정보">
-        <ul className="flex w-full gap-[10]" aria-label="카드번호">
+        <ul className="flex w-full h-5 gap-[10]" aria-label="카드번호">
           {cardNumbers.map((number, index) => (
-            <p key={index} className="flex-1 font-medium text-sm/5 text-white tracking-widest text-center">
+            <p key={index} className="flex-1 font-medium text-sm/5 text-white tracking-widest">
               {index >= 2 ? '*'.repeat(number.length) : number}
             </p>
           ))}
         </ul>
-        <p className="flex-1 font-medium text-sm/5 text-white tracking-widest" aria-label="유효기간">
+        <p className="flex-1 h-5 font-medium text-sm/5 text-white tracking-widest" aria-label="유효기간">
           {expirationDate.every(expiration => expiration === '') ? '' : expirationDate.join('/')}
         </p>
-        <p className="flex-1 font-medium text-sm/5 text-white tracking-widest" aria-label="소유자명">
+        <p className="flex-1 h-5 font-medium text-sm/5 text-white tracking-widest" aria-label="소유자명">
           {cardName.toUpperCase()}
         </p>
       </section>
